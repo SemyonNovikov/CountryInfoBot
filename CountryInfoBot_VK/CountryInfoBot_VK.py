2707f290e377db01704e468a9c09e96605b3263a
Semyon Novikov <semyon_novikov@github.com>
2018-05-12 22:53:41 +0300

    Add comments - добавил комментарии

--- CountryInfoBot_VK/CountryInfoBot_VK.py
+++ CountryInfoBot_VK/CountryInfoBot_VK.py
@@ -1,4 +1,5 @@
-# VERSION BOT : 0.1.1 
+# BOT VERSION : 0.1.2
+
 import os
 import json
 import requests
@@ -20,7 +21,7 @@ translate = YandexTranslate('API KEY')
 token_vk = 'API KEY'
 
 # ключ для конвертации через api cloudconvert
-api = cloudconvert.Api('API KEY')  
+api = cloudconvert.Api('API KEY')   
 
 # мой переводчик
 my_translate = {   
@@ -63,52 +64,51 @@ vk = vk_session.get_api()
 upload = VkUpload(vk_session)      # для загрузки изображений
 longpoll = VkLongPoll(vk_session)  # инициализация лонгполлинга
 
-def translator(text,direction):  # text - что перевести , direction - направление перевода
+def translator(text,direction):  # Функция переводчик , аргументы : text - что перевести , direction - направление перевода
 	try:
-		perevod = ('Translate:', translate.translate(text, direction))  # or just 'en'
+		perevod = ('Translate:', translate.translate(text, direction))
 		perevod = (perevod[1])
-		perevod = perevod["text"]
+		perevod = perevod["text"]  # получаем текст перевода
 		country = ''.join(perevod)
 	except:
 		print('Ошибка при переводе !')
 		country = "error"
-	return country
+	return country  # возвращаем страну на английском 
 
 def state0(message: Event):
-	text = message.text
-	hello = text.lower()
+
+	text = message.text  # сообщение от пользователя хранится в переменной text
+	hello = text.lower() 
 	
 	if hello == 'привет':
 		text = 'Привет! Я справочник по странам . Напиши мне, например "Столица Франции" или "Население США" . Чтобы узнать полный список команд напиши "что ты умеешь" .'
 	elif hello == 'что ты умеешь' or hello == 'что ты умеешь?':
 		text = 'Как пользоваться ботом : \n"команда страна"\n\nНапример : Флаг США , Народ Аргентины и тд. \n\nПолный список команд : \n\n' 
-		for key in my_translate.keys():
+		for key in my_translate.keys(): # выводим все команды из словаря
 			text+= key + ' , '
-		text = text[0:-2]
-		text+=' .'
+		text = text[0:-2]  # убираем лишнюю запятую 
+		text+=' .'		   # и меняем её на точку
 	else:
 		try:
 			a = text.split()
 			find_ru = a[0]
-			find_en = my_translate[find_ru.lower()]
+			find_en = my_translate[find_ru.lower()]  # переводим команду с помощью словаря 'my_translate'
 
 			print('------------------------------------------------')
-			print('Что найти :',find_en)
+			print('Что найти :', find_en)
 
 			# извините, здесь костыль из-за неправильного перевода YandexTranslate
-			if a[1] == "Того":  
+			if a[1].lower() == "того":  
 				country="Togo"
 			else:
 				try:
-					country = translator(a[1]+' '+a[2], 'en')
+					country = translator(a[1]+' '+a[2], 'en') # если страна состоит из двух слов
 				except IndexError:
-					country = translator(a[1], 'en')
+					country = translator(a[1], 'en')  # и если из одного
 
-			print('Страна :',country)
+			print('Страна :', country)
 
-			#https://restcountries.eu/rest/v2/name/russia/?fields=name;capital;currencies  # пример запроса по отдельным полям
-
-			url = 'https://restcountries.eu/rest/v2/name/' + country + '/?fields=' + find_en + ';'
+			url = 'https://restcountries.eu/rest/v2/name/' + country + '/?fields=' + find_en + ';' # country - страна , find_en - что надо найти
 			response = requests.get(url)
 			list = (response.json()[0])					   # получаем список
 			print(list)
@@ -117,19 +117,20 @@ def state0(message: Event):
 				otvet = ''
 				dict = list[find_en]
 				print(dict)
-				for l in dict:
+				for l in dict:  # так как языков в стране может быть несколько мы пробегаемся по всем
 					print(l)
 					otvet += translator(l['name'],'en-ru') + ' , '
-				otvet = otvet[0:-2]
+
+				otvet = otvet[0:-2]   # убираем лишнюю запятую и меняем её на точку
 				otvet+=' .'
 
-				#print('Валюта или язык :', otvet)
+				print('Валюта или язык :', otvet)
 				text = text + " - " + str(otvet)
 
 			elif find_en == 'numericCode' or find_en == 'nativeName':
 
 				otvet = list[find_en]
-				text = text + " - " + str(otvet)
+				text = text + " - " + str(otvet) # numericCode и nativeName переводить не надо
 
 			elif find_en == 'timezones' or find_en == 'borders':
 
@@ -138,7 +139,7 @@ def state0(message: Event):
 
 				for i in otvet:
 					text+= i + ' , '
-				text = text[0:-2]
+				text = text[0:-2]   # убираем лишнюю запятую и меняем её на точку
 				text+=' .'
 
 			elif find_en == 'population':
@@ -162,13 +163,15 @@ def state0(message: Event):
 
 			elif find_en == 'flag':
 
+				# путь для JPG картинки
 				path_to_jpg = 'C:/Main/Programming/PythonProjects/CountryInfoBot_VK/Photos/'+country+'.jpg'
+				# путь для SVG картинки
 				path_to_svg = 'C:/Main/Programming/PythonProjects/CountryInfoBot_VK/Photos/'+country+'.svg'
 
-				if os.path.exists(path_to_jpg):
+				if os.path.exists(path_to_jpg):  # проверяем существует ли картинка
 					print('Картинка уже есть')
-				else:
-					print('Файл не существует')
+				else:							 # иначе скачиваем и конвертируем её
+					print('Картинка не существует')
 
 					url = str(list[find_en])
 					print(url)
@@ -188,11 +191,17 @@ def state0(message: Event):
 							"resize": "500x500",   # обрезаем картинку до 500х500
 						}
 					})
-					process.wait() # wait until conversion finished
-					process.download(path_to_jpg) # download output file
-					os.remove(path_to_svg)  #delete svg file - удаляем svg фото после конвертации
+					
+					# wait until conversion finished - ждём окончание конвертации
+					process.wait() 
 
-					print("конвертировали SVG картинку в JPEG")
+					# download output file - скачиваем JPG картинку
+					process.download(path_to_jpg)
+
+					# delete SVG image - удаляем SVG картинку после конвертации
+					os.remove(path_to_svg)  
+
+					print("конвертировали SVG картинку в JPG")
 
 				photo = upload.photo_messages(photos=path_to_jpg)[0]
 
@@ -201,21 +210,22 @@ def state0(message: Event):
 				text = text + " - " + str(otvet)
 		except:
 
-			print("Ошибка!")
-			text = 'Такой команды нет ! Чтобы узнать список команд напиши : "что ты умеешь" '
+			print("Ошибка !")
+			text = 'Такой команды или страны нет ! Чтобы узнать список команд напиши : "что ты умеешь" '
 
 	try:
 		# отправляем сообщение
 		vk.messages.send(
-		user_id = message.user_id,				 # кому
-		message = text,							 # сообщение хранится в переменной 'text'
-		attachment = 'photo{}_{}'.format(photo['owner_id'], photo['id'])
+		user_id = message.user_id,	# кому
+		message = text,  # сообщение хранится в переменной 'text'
+		attachment = 'photo{}_{}'.format(photo['owner_id'], photo['id'])  # send image - отправляем картинку 
 		)
+
 	except:
 		# отправляем сообщение
 		vk.messages.send(
-		user_id = message.user_id,				# кому
-		message = text							# сообщение хранится в переменной 'text'
+		user_id = message.user_id,		# кому
+		message = text					# сообщение хранится в переменной 'text'
 		)
 
 # ожидаем события
